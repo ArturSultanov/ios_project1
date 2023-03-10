5aa0fa1a709d27021b99c764609d3ab6da30ee8f
Sultanov Artur <xsulta01@merlin.fit.vutbr.cz>
2023-03-10 23:25:16 +0100

    update

--- mole.sh
+++ mole.sh
@@ -3,36 +3,13 @@
 export POSIXLY_CORRECT=yes
 export LC_NUMERIC=en_US.UTF-8
 
-function check(){
-	echo hi	
-}
+##### FUNCTIONS #####
 
-if [ -z $MOLE_RC ]; then
-	echo "MOLE_RC neexistuje" >&2
-	exit 1
-fi
-
-if [ ! -f $MOLE_RC ]; then
-		touch "$MOLE_RC"
-		echo "MOLE_RC byl vytvoren"
-fi 
-
-lastArg=
-
-eval lastArg='$'$# 				#get last argument
-
-#'vi' $lastArg
-
- 
-
-#Open file using EDITOR, VISUAL or vi
-if [ $# ]; then				#check if user has typed any argumnts 
+function openEditor () { #Open file using EDITOR, VISUAL or vi.
 	if [ -f $lastArg ]; then	
-	
 		if [ -z  $EDITOR ]; then
-			echo kek
-			if [ -z $VISUAL ]; then
-				'vi' $lastArg		#using vi as default option
+			if [ -z $VISUAL ]; then	
+				eval 'vi'  $lastArg
 			else
 				eval '$VISUAL' $lastArg
 			fi
@@ -40,15 +17,32 @@ if [ $# ]; then				#check if user has typed any argumnts
 			eval '$EDITOR' $lastArg
 		fi
 	fi		
-else 
-	echo "Nezadal jste zadny argument"
+}
+
+##### CONDITION CHECK #####
+
+if [ -z $MOLE_RC ]; then
+	echo "MOLE_RC neexistuje" >&2
 fi
 
+if [ ! -f $MOLE_RC ]; then
+		touch "$MOLE_RC"
+		echo "MOLE_RC byl vytvoren"
+fi 
 
 
+if [ $# -eq 0 ]; then
+	echo "You didn't enter any argument" >&2
+	echo "Use '-h' to get help"
+fi
 
-while getopts 'hg:a:b:' OPTION; do
-	case "$OPTION" in
+##### MAIN #####
+
+
+eval lastArg='$'$#
+
+while getopts :hg::m OPTION; 
+do	case "$OPTION" in
 	h)
 	 echo "
 		mole -h
@@ -57,17 +51,15 @@ while getopts 'hg:a:b:' OPTION; do
 		mole list [FILTERS] [DIRECTORY]" ;;
  
     	g) 
-      	 echo "Option a used" ;;
+      	 echo "Option a used" | openEditor;;    	
 
-    	b)
-      	 echo "Option b used" ;;
-
-    	c)
+    	m)
       	 echo "Option c used" ;;	
 
-	?) 
-      	 echo "WOW!"
-      	 #exit 1
+	*) 
       	 ;;
       esac
    done
+
+#shift $OPTIND
+#echo "Remaining arguments: '$*'"
